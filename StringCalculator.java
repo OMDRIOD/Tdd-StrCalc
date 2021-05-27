93d38f61e4ed2ebde79bb06c2bd1d02214b16717
Omkar <kartikpunekar7@gmail.com>
2021-05-27 17:28:01 +0530

    Knowing the delimiter

--- StringCalculator.java
+++ StringCalculator.java
@@ -4,12 +4,29 @@ public class StringCalculator {
     public int Add(String numbers) throws Exception {
 		if(numbers.length()==0) return 0;
 		
-		// Sanitize the input String
 		String[] operators = new String[] {"\\+","\\*","x","\\^","X"};
 		for(String operator:operators) {
 			numbers = numbers.replaceAll("["+operator+"+]", ";");
 		}
+        String delimiter = ",";
+		if(numbers.startsWith("//")) {
+			if(numbers.contains("[") && numbers.contains("]")) {
+				while(numbers.contains("[") && numbers.contains("]")) {
+					int startIndex = numbers.indexOf("[");
+					int endIndex = numbers.indexOf("]");
+					delimiter = numbers.substring(startIndex+1, endIndex);
+					numbers = numbers.substring(endIndex+1).replaceAll("["+delimiter+"+]", ";");
+					delimiter = ";";
+				}
+			}else {
+				delimiter = ""+numbers.charAt(2);
+				numbers = numbers.substring(3);
+			}
+		}
+
+
         return 0;
+
     }
     public static void main(String[] args) {
         
